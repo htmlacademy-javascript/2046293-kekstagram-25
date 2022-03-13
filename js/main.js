318be723d5eaeb11d6984682ffad964dc58f0dc6
Jenya <Ywiko56@gmail.com>
2022-03-13 16:00:15 +0300

    исправил примечания

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 function getRandomInt (min, max) {                 //Информацию взял из источника: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-  if (max>=min){return 'Неверный диапазон';
+  if (min > max){
+    return 'Неверный диапазон';
   }
+
   min = Math.ceil(min);
   max = Math.floor(max);
+
   return Math.floor(Math.random()* (max - min + 1)) + min;
 }
 getRandomInt();
 
-function commentsValid(string,max){
-  if (string.lenght > max) {return false;}
+function commentsValid(string,max) {
+  if (string.lenght > max) {
+    return false;
+  }
+
   return true;
 }
-commentsValid();
+commentsValid('Длинна строки', 5);
+
