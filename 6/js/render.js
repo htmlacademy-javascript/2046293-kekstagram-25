fa51468f6f7e84a229de141d3bdeaa92e1b9b83e
Keks <keks@htmlacademy.ru>
2022-03-30 21:07:56 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/render.js
+++ 6/js/render.js
@@ -1,16 +1,19 @@
 import {createUsersProfiles} from './data.js';
 
 const pictureListElement = document.querySelector('.pictures');
-const pictureTemplate = document.querySelector('#picture');
-const pictureRendering = createUsersProfiles();
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const pictureRendering = createUsersProfiles(25);
 
 const pictureListFragment = document.createDocumentFragment();
 
-pictureRendering.forEach(({url, likes, comments}) => {
+pictureRendering.forEach(({url, comments, likes}) => {
   const  pictureElement = pictureTemplate.cloneNode(true);
-  pictureElement.querySelector('.picture__img').src.textContent = url;
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
   pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.querySelector('.picture__comments').textContent = comments;
   pictureListFragment.appendChild(pictureElement);
 });
 pictureListElement.appendChild(pictureListFragment);
